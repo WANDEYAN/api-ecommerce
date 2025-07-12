acd5f0415b0ab1f8b61dacacd3e1007fc8e72cc5
wande <wandev.web@gmail.com>
2025-07-12 16:51:02 -0300

    Add REST controller and hello endpoint to ApiApplication for test first start application.

--- src/main/java/br/com/ecommerce/api/ApiApplication.java
+++ src/main/java/br/com/ecommerce/api/ApiApplication.java
@@ -2,12 +2,20 @@ package br.com.ecommerce.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController("/")
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello, World!";
 	}
 
 }
