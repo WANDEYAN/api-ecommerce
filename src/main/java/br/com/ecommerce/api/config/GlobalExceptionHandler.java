b5cedd32c6b4656f761d44b489ee54f4fb2fbce2
wande <wandev.web@gmail.com>
2025-07-14 12:36:38 -0300

    refactor: add custom exception handler for user registration

---
+++ src/main/java/br/com/ecommerce/api/config/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package br.com.ecommerce.api.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import br.com.ecommerce.api.service.exceptions.EmailAlreadyExistsException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ResponseEntity<String> handleEmailAlreadyExists(EmailAlreadyExistsException ex){
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+    }
+}
